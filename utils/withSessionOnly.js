11285184cb168339006a6d3e692b3f6699c57394
Aapeli Haanpuu <aapeli.haanpuu@gofore.com>
2019-08-04 22:54:49 +0300

    Bugfix

--- utils/withSessionOnly.js
+++ utils/withSessionOnly.js
@@ -1,16 +1,14 @@
 const { getSession } = require('./current-session')
 const authenticatedOnly = require('./authenticatedOnly')
 
-const bot = require('../bot')
-
-const withSessionOnly = (func) => authenticatedOnly((msg, match) => {
+const withSessionOnly = (func) => authenticatedOnly((msg, reply) => {
     const chatId = msg.chat.id
     const sessionHash = getSession(chatId)
     if (!sessionHash) {
         return reply.text('You have to open or create a session first')
     }
     msg.sessionHash = sessionHash
-    return func(msg, match)
+    return func(msg, reply)
 })
 
 module.exports = withSessionOnly
